caaa7bb77935ca4720e46711453af253c1f1dbe2
杨国保 <yangguobao@edaijia-inc.cn>
2021-02-22 18:34:35 +0800

    烟花finish

--- fireworks/js/index.js
+++ fireworks/js/index.js
@@ -1,75 +1,406 @@
-(function() {
-    // let canvas = document.getElementById('canvas');
-    // let context = canvas.getContext('2d');
-    /*let count = 10; // 烟花粒子数量
-    let initialRadius = 10; // 烟花环绕半径
-    let particles = [];
-    let point = {
-        x: 0,
-        y: 0,
-        radius: 10,
-        count: 10,
-        timer: null
+let Fireworks = function () {
+    /*=============================================================================*/
+    let self = this;
+    let rand = function (rMi, rMa) {
+        return ~~((Math.random() * (rMa - rMi + 1)) + rMi);
     }
+    let hitTest = function (x1, y1, w1, h1, x2, y2, w2, h2) {
+        return !(x1 + w1 < x2 || x2 + w2 < x1 || y1 + h1 < y2 || y2 + h2 < y1);
+    };
+    window.requestAnimFrame = function () {
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (a) {
+                window.setTimeout(a, 1E3 / 60)
+            }
+        )
+    }();
 
-    function resizeCanvas() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    }
+    /*=============================================================================*/
+    /* Initialize
+    /*=============================================================================*/
+    self.init = function () {
+        self.dt = 0;
+        self.oldTime = Date.now();
+        self.canvas = document.createElement('canvas');
+        self.canvasContainer = $('#canvasBox');
 
-    function clearCanvas() {
-        context.fillStyle = '#ffffff';
-        context.fillRect(0, 0, canvas.width, canvas.height);
-    }
+        self.canvas.onselectstart = function () {
+            return false;
+        };
 
-    window.addEventListener('resize', resizeCanvas);
-    resizeCanvas();
+        self.canvas.width = self.cw = window.innerWidth;
+        self.canvas.height = self.ch = window.innerHeight;
 
-    canvas.addEventListener('mousedown', handleMousedown);
+        self.particles = [];
+        self.partCount = 30;
+        self.fireworks = [];
+        self.mx = self.cw / 2;
+        self.my = self.ch / 2;
+        self.currentHue = 170;
+        self.partSpeed = 5;
+        self.partSpeedVariance = 10;
+        self.partWind = 50;
+        self.partFriction = 5;
+        self.partGravity = 1;
+        self.hueMin = 150;
+        self.hueMax = 200;
+        self.fworkSpeed = 2;
+        self.fworkAccel = 4;
+        self.hueVariance = 30;
+        self.flickerDensity = 20;
+        self.showShockwave = false;
+        self.showTarget = true;
+        self.clearAlpha = 25;
 
-    function handleMousedown(e) {
-        let x = e.clientX;
-        let y = e.clientY;
+        self.canvasContainer.append(self.canvas);
+        self.ctx = self.canvas.getContext('2d');
+        self.ctx.lineCap = 'round';
+        self.ctx.lineJoin = 'round';
+        self.lineWidth = 1;
+        self.bindEvents();
+        self.canvasLoop();
 
-        let p = {
-            ...point,
-            x,
-            y
+    };
+
+    /*=============================================================================*/
+    /* Particle Constructor
+    /*=============================================================================*/
+    let Particle = function (x, y, hue) {
+        this.x = x;
+        this.y = y;
+        this.coordLast = [
+            {x: x, y: y},
+            {x: x, y: y},
+            {x: x, y: y}
+        ];
+        this.angle = rand(0, 360);
+        this.speed = rand(((self.partSpeed - self.partSpeedVariance) <= 0) ? 1 : self.partSpeed - self.partSpeedVariance, (self.partSpeed + self.partSpeedVariance));
+        this.friction = 1 - self.partFriction / 100;
+        this.gravity = self.partGravity / 2;
+        this.hue = rand(hue - self.hueVariance, hue + self.hueVariance);
+        this.brightness = rand(50, 80);
+        this.alpha = rand(40, 100) / 100;
+        this.decay = rand(10, 50) / 1000;
+        this.wind = (rand(0, self.partWind) - (self.partWind / 2)) / 25;
+        this.lineWidth = self.lineWidth;
+    };
+
+    Particle.prototype.update = function (index) {
+        let radians = this.angle * Math.PI / 180;
+        let vx = Math.cos(radians) * this.speed;
+        let vy = Math.sin(radians) * this.speed + this.gravity;
+        this.speed *= this.friction;
+
+        this.coordLast[2].x = this.coordLast[1].x;
+        this.coordLast[2].y = this.coordLast[1].y;
+        this.coordLast[1].x = this.coordLast[0].x;
+        this.coordLast[1].y = this.coordLast[0].y;
+        this.coordLast[0].x = this.x;
+        this.coordLast[0].y = this.y;
+
+        this.x += vx * self.dt;
+        this.y += vy * self.dt;
+
+        this.angle += this.wind;
+        this.alpha -= this.decay;
+
+        if (!hitTest(0, 0, self.cw, self.ch, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2) || this.alpha < .05) {
+            self.particles.splice(index, 1);
         }
+    };
 
-        fire(p);
-    }
+    Particle.prototype.draw = function () {
+        let coordRand = (rand(1, 3) - 1);
+        self.ctx.beginPath();
+        self.ctx.moveTo(Math.round(this.coordLast[coordRand].x), Math.round(this.coordLast[coordRand].y));
+        self.ctx.lineTo(Math.round(this.x), Math.round(this.y));
+        self.ctx.closePath();
+        self.ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')';
+        self.ctx.stroke();
 
-    function draw(p) {
-        clearCanvas();
-
-        for (let i = 0; i < p.count; i++) {
-            let angle = 360/p.count * i; // 烟花粒子角度
-            let radian = angle * Math.PI / 180; // 烟花粒子弧度
-
-            let vx = p.x + Math.cos(radian) * p.radius;
-            let vy = p.y + Math.sin(radian) * p.radius;
-
-            let size = 2;
-            context.beginPath();
-            context.arc(vx, vy, size, 0, Math.PI*2)
-            context.closePath();
-            context.fillStyle = "#ff0000";
-            context.fill();
-        }
-    }
-
-    function fire(p) {
-        function tick() {
-            draw(p); // 绘制烟花
-            if (p.radius >= 100) {
-                clearInterval(p.timer)
-            } else {
-                p.radius++
+        if (self.flickerDensity > 0) {
+            let inverseDensity = 50 - self.flickerDensity;
+            if (rand(0, inverseDensity) === inverseDensity) {
+                self.ctx.beginPath();
+                self.ctx.arc(Math.round(this.x), Math.round(this.y), rand(this.lineWidth, this.lineWidth + 3) / 2, 0, Math.PI * 2, false)
+                self.ctx.closePath();
+                let randAlpha = rand(50, 100) / 100;
+                self.ctx.fillStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + randAlpha + ')';
+                self.ctx.fill();
             }
         }
-        tick();
-        p.timer = setInterval(tick, 30); // 每30毫秒绘制一次
-    }*/
+    };
 
-})()
+    /*=============================================================================*/
+    /* Create Particles
+    /*=============================================================================*/
+    self.createParticles = function (x, y, hue) {
+        let countdown = self.partCount;
+        while (countdown--) {
+            self.particles.push(new Particle(x, y, hue));
+        }
+    };
+
+    /*=============================================================================*/
+    /* Update Particles
+    /*=============================================================================*/
+    self.updateParticles = function () {
+        let i = self.particles.length;
+        while (i--) {
+            let p = self.particles[i];
+            p.update(i);
+        }
+    };
+
+    /*=============================================================================*/
+    /* Draw Particles
+    /*=============================================================================*/
+    self.drawParticles = function () {
+        let i = self.particles.length;
+        while (i--) {
+            let p = self.particles[i];
+            p.draw();
+        }
+        ;
+    };
+
+    /*=============================================================================*/
+    /* Firework Constructor
+    /*=============================================================================*/
+    let Firework = function (startX, startY, targetX, targetY) {
+        this.x = startX;
+        this.y = startY;
+        this.startX = startX;
+        this.startY = startY;
+        this.hitX = false;
+        this.hitY = false;
+        this.coordLast = [
+            {x: startX, y: startY},
+            {x: startX, y: startY},
+            {x: startX, y: startY}
+        ];
+        this.targetX = targetX;
+        this.targetY = targetY;
+        this.speed = self.fworkSpeed;
+        this.angle = Math.atan2(targetY - startY, targetX - startX);
+        this.shockwaveAngle = Math.atan2(targetY - startY, targetX - startX) + (90 * (Math.PI / 180));
+        this.acceleration = self.fworkAccel / 100;
+        this.hue = self.currentHue;
+        this.brightness = rand(50, 80);
+        this.alpha = rand(50, 100) / 100;
+        this.lineWidth = self.lineWidth;
+        this.targetRadius = 1;
+    };
+
+    Firework.prototype.update = function (index) {
+        self.ctx.lineWidth = this.lineWidth;
+
+        vx = Math.cos(this.angle) * this.speed,
+            vy = Math.sin(this.angle) * this.speed;
+        this.speed *= 1 + this.acceleration;
+        this.coordLast[2].x = this.coordLast[1].x;
+        this.coordLast[2].y = this.coordLast[1].y;
+        this.coordLast[1].x = this.coordLast[0].x;
+        this.coordLast[1].y = this.coordLast[0].y;
+        this.coordLast[0].x = this.x;
+        this.coordLast[0].y = this.y;
+
+        if (self.showTarget) {
+            if (this.targetRadius < 8) {
+                this.targetRadius += .25 * self.dt;
+            } else {
+                this.targetRadius = 1 * self.dt;
+            }
+        }
+
+        if (this.startX >= this.targetX) {
+            if (this.x + vx <= this.targetX) {
+                this.x = this.targetX;
+                this.hitX = true;
+            } else {
+                this.x += vx * self.dt;
+            }
+        } else {
+            if (this.x + vx >= this.targetX) {
+                this.x = this.targetX;
+                this.hitX = true;
+            } else {
+                this.x += vx * self.dt;
+            }
+        }
+
+        if (this.startY >= this.targetY) {
+            if (this.y + vy <= this.targetY) {
+                this.y = this.targetY;
+                this.hitY = true;
+            } else {
+                this.y += vy * self.dt;
+            }
+        } else {
+            if (this.y + vy >= this.targetY) {
+                this.y = this.targetY;
+                this.hitY = true;
+            } else {
+                this.y += vy * self.dt;
+            }
+        }
+
+        if (this.hitX && this.hitY) {
+            let randExplosion = rand(0, 9);
+            self.createParticles(this.targetX, this.targetY, this.hue);
+            self.fireworks.splice(index, 1);
+        }
+    };
+
+    Firework.prototype.draw = function () {
+        self.ctx.lineWidth = this.lineWidth;
+
+        let coordRand = (rand(1, 3) - 1);
+        self.ctx.beginPath();
+        self.ctx.moveTo(Math.round(this.coordLast[coordRand].x), Math.round(this.coordLast[coordRand].y));
+        self.ctx.lineTo(Math.round(this.x), Math.round(this.y));
+        self.ctx.closePath();
+        self.ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')';
+        self.ctx.stroke();
+
+        if (self.showTarget) {
+            self.ctx.save();
+            self.ctx.beginPath();
+            self.ctx.arc(Math.round(this.targetX), Math.round(this.targetY), this.targetRadius, 0, Math.PI * 2, false)
+            self.ctx.closePath();
+            self.ctx.lineWidth = 1;
+            self.ctx.stroke();
+            self.ctx.restore();
+        }
+
+        if (self.showShockwave) {
+            self.ctx.save();
+            self.ctx.translate(Math.round(this.x), Math.round(this.y));
+            self.ctx.rotate(this.shockwaveAngle);
+            self.ctx.beginPath();
+            self.ctx.arc(0, 0, 1 * (this.speed / 5), 0, Math.PI, true);
+            self.ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + rand(25, 60) / 100 + ')';
+            self.ctx.lineWidth = this.lineWidth;
+            self.ctx.stroke();
+            self.ctx.restore();
+        }
+    };
+
+    /*=============================================================================*/
+    /* Create Fireworks
+    /*=============================================================================*/
+    self.createFireworks = function (startX, startY, targetX, targetY) {
+        self.fireworks.push(new Firework(startX, startY, targetX, targetY));
+    };
+
+    /*=============================================================================*/
+    /* Update Fireworks
+    /*=============================================================================*/
+    self.updateFireworks = function () {
+        let i = self.fireworks.length;
+        while (i--) {
+            let f = self.fireworks[i];
+            f.update(i);
+        }
+        ;
+    };
+
+    /*=============================================================================*/
+    /* Draw Fireworks
+    /*=============================================================================*/
+    self.drawFireworks = function () {
+        let i = self.fireworks.length;
+        while (i--) {
+            let f = self.fireworks[i];
+            f.draw();
+        }
+        ;
+    };
+
+    /*=============================================================================*/
+    /* Events
+    /*=============================================================================*/
+    self.bindEvents = function () {
+        $(window).on('resize', function () {
+            clearTimeout(self.timeout);
+            self.timeout = setTimeout(function () {
+                self.ctx.lineCap = 'round';
+                self.ctx.lineJoin = 'round';
+            }, 100);
+        });
+
+        $(self.canvas).on('mousedown', function (e) {
+            let randLaunch = rand(0, 5);
+            self.mx = e.pageX - self.canvasContainer.offset().left;
+            self.my = e.pageY - self.canvasContainer.offset().top;
+            self.currentHue = rand(self.hueMin, self.hueMax);
+            self.createFireworks(self.cw / 2, self.ch, self.mx, self.my);
+
+            $(self.canvas).on('mousemove.fireworks', function (e) {
+                let randLaunch = rand(0, 5);
+                self.mx = e.pageX - self.canvasContainer.offset().left;
+                self.my = e.pageY - self.canvasContainer.offset().top;
+                self.currentHue = rand(self.hueMin, self.hueMax);
+                self.createFireworks(self.cw / 2, self.ch, self.mx, self.my);
+            });
+
+        });
+
+        $(self.canvas).on('mouseup', function (e) {
+            $(self.canvas).off('mousemove.fireworks');
+        });
+
+    }
+
+    /*=============================================================================*/
+    /* Clear Canvas
+    /*=============================================================================*/
+    self.clear = function () {
+        self.particles = [];
+        self.fireworks = [];
+        self.ctx.clearRect(0, 0, self.cw, self.ch);
+    };
+
+    /*=============================================================================*/
+    /* Delta
+    /*=============================================================================*/
+    self.updateDelta = function () {
+        let newTime = Date.now();
+        self.dt = (newTime - self.oldTime) / 16;
+        self.dt = (self.dt > 5) ? 5 : self.dt;
+        self.oldTime = newTime;
+    }
+
+    /*=============================================================================*/
+    /* Main Loop
+    /*=============================================================================*/
+    self.canvasLoop = function () {
+        requestAnimFrame(self.canvasLoop, self.canvas);
+        self.updateDelta();
+        self.ctx.globalCompositeOperation = 'destination-out';
+        self.ctx.fillStyle = 'rgba(0,0,0,' + self.clearAlpha / 100 + ')';
+        self.ctx.fillRect(0, 0, self.cw, self.ch);
+        self.ctx.globalCompositeOperation = 'lighter';
+        self.updateFireworks();
+        self.updateParticles();
+        self.drawFireworks();
+        self.drawParticles();
+    };
+
+    self.init();
+
+    let initialLaunchCount = 10;
+    while (initialLaunchCount--) {
+        setTimeout(function () {
+            self.fireworks.push(new Firework(self.cw / 2, self.ch, rand(50, self.cw - 50), rand(50, self.ch / 2) - 50));
+        }, initialLaunchCount * 200);
+    }
+
+}
+
+new Fireworks();
